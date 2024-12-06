5ad5b3f61ac6738d35e12a056e62b2b48216aa64
Schr0Sm1ley <radiantremel444@gmail.com>
2024-12-06 23:55:55 +0600

    Implemented all the functionalities properly, need optimization

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@schr0smi1ey.iioky.mongodb.net/?retryWrites=true&w=majority&appName=Schr0Smi1ey`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,8 +25,66 @@ const client = new MongoClient(uri, {
   },
 });
 
+const database = client.db("VisaEase");
+const visaCollection = database.collection("visas");
+const applicationCollection = database.collection("applications");
 async function run() {
   try {
+    // Post
+    app.post("/Visa", async (req, res) => {
+      const visa = req.body;
+      const result = await visaCollection.insertOne(visa);
+      res.send(result);
+    });
+    // Get
+    app.get("/Visa", async (req, res) => {
+      const cursor = visaCollection.find();
+      const visas = await cursor.toArray();
+      res.send(visas);
+    });
+    app.get("/Visa/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const visa = await visaCollection.findOne(query);
+      res.send(visa);
+    });
+    app.put("/Visa/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const visa = req.body;
+      console.log(visa);
+      const update = { $set: visa };
+      const result = await visaCollection.updateOne(query, update);
+      res.send(result);
+    });
+    app.delete("/Visa/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await visaCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.post("/Applications", async (req, res) => {
+      const application = req.body;
+      const result = await applicationCollection.insertOne(application);
+      res.send(result);
+    });
+    app.get("/Applications", async (req, res) => {
+      const cursor = applicationCollection.find();
+      const applications = await cursor.toArray();
+      res.send(applications);
+    });
+    app.get("/Applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const application = await applicationCollection.findOne(query);
+      res.send(application);
+    });
+    app.delete("/Applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await applicationCollection.deleteOne(query);
+      res.send(result);
+    });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
